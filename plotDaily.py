d8230bcd2869620cc06e4d08b4c79de3f8b63370
DrRobbe <marcelwallraff@gmail.com>
2023-11-20 22:38:38 +0100

    flake8

--- plotDaily.py
+++ plotDaily.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def plot_daily(source_file):
     dates, upvotes, comments, awards, snap_value, snapshots, weekend = get_plot_data(source_file)
     plt.title('Daily Discussion comments')
-    plt.plot(dates,comments, label="comments")
+    plt.plot(dates, comments, label="comments")
     legened_snapshot = True
     for idx, snapshot in enumerate(snapshots):
         if legened_snapshot:
@@ -21,12 +22,13 @@ def plot_daily(source_file):
             legened_weekend = False
         else:
             plt.axvspan(day-1, day, alpha=0.2, color='green')
-            
+
     plt.xticks(np.arange(0, len(dates), 5.0))
-    plt.xlabel('date') 
-    plt.ylabel('comments') 
+    plt.xlabel('date')
+    plt.ylabel('comments')
     plt.legend()
     plt.show()
+
 
 def get_plot_data(source_file):
     dates = []
@@ -38,7 +40,7 @@ def get_plot_data(source_file):
     weekend = []
     data_file = open(source_file, "r")
     data_lines = data_file.readlines()
-    iterator = 1 
+    iterator = 1
     for line in data_lines:
         sub_list = line.split('#')
         tmp_date = sub_list[0].split(',')[0].split(' ')
@@ -52,9 +54,10 @@ def get_plot_data(source_file):
                 snap_value.append(sub_list[4].split("-")[1])
             if "weekend" in sub_list[5]:
                 weekend.append(iterator)
-        iterator += 1       
+        iterator += 1
     data_file.close()
     return dates, upvotes, comments, awards, snap_value, snapshots, weekend
+
 
 if __name__ == "__main__":
     plot_daily("D:\\Scripts\\Data\\Daily.txt")

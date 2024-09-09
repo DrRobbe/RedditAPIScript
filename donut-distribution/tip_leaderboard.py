01cee06f57562bf4a7d61407b85e9d09118a1bc6
DrRobbe <marcelwallraff@gmail.com>
2024-09-09 15:04:24 +0200

    More stats

--- donut-distribution/tip_leaderboard.py
+++ donut-distribution/tip_leaderboard.py
@@ -81,19 +81,37 @@ def create_table(users: Dict[str, Dict[str, List[float]]], send_table: bool, dat
 
 if __name__ == "__main__":
     local_path = 'D:\\Scripts\\RedditAPIScript\\donut-distribution\\'
-    date = datetime.strptime("2024-07-31 00:00:00", '%Y-%m-%d %H:%M:%S')
+    date = datetime.strptime("2024-09-02 00:00:00", '%Y-%m-%d %H:%M:%S')
     print("Check all tips since :" + str(date))
-    file_name = local_path + 'input\\tips_round_140.json'
+    file_name = local_path + 'input\\tips_round_141.json'
     user_send, user_receive = create_user(file_name, date)
     # global data
     all_tips = 0
     all_donuts = 0.
-    for _, data in user_send.items():
-        for _, values in data.items():
+    max_donuts = 0.
+    max_send_donuts = ''
+    max_received_donuts = ''
+    max_tips = 0
+    max_send_tip = ''
+    max_received_tip = ''
+    for send_user, data in user_send.items():
+        for receive_user, values in data.items():
             all_tips += values[0]
             all_donuts += values[1]
+            if max_donuts < values[1]:
+                max_donuts = values[1]
+                max_send_donuts = send_user
+                max_received_donuts = receive_user
+            if max_tips < values[0]:
+                max_tips = values[0]
+                max_send_tip = send_user
+                max_received_tip = receive_user            
     print(f"{all_tips} tips send")
-    print(f"{round(all_donuts, 1)} donuts send")  
+    print(f"On average {round(all_tips/len(user_send), 1)} tips were send per user")
+    print(f"{round(all_donuts, 1)} donuts send")
+    print(f"On average {round(all_donuts/len(user_send), 1)} donuts were send per user")
+    print(f"Most tips send this week from one person to another: {max_send_tip} send {max_tips} tips to {max_received_tip}")
+    print(f"Most donuts send this week from one person to another: {max_send_donuts} send {round(max_donuts, 1)} donuts to {max_received_donuts}")
     send_ranks = create_table(user_send, True, date)
     received_ranks = create_table(user_receive, False, date)
     # calculate rank differenc

423a24f032a3a01e68c4b75a570c1c55e111825a
DrRobbe <marcelwallraff@gmail.com>
2024-10-08 07:28:01 +0200

    add contrib to pow analysis

--- donut-distribution/pow_analysis.py
+++ donut-distribution/pow_analysis.py
@@ -17,11 +17,12 @@ if __name__ == "__main__":
                         if author not in all_users:
                             all_users[author] = [0,0,0,0]   
                         all_users[author][values["rank"]-1] += 1
-    output = [f"| No. | Name | Rank 1 | Rank 2 | Rank 3 | Rank 4 |",
-              "|:-|:--------------|:-------:|:---------------------:|:------:|:---------------------:|"]
+    output = [f"| No. | Name | Contrib gained | Rank 1 | Rank 2 | Rank 3 | Rank 4 | ",
+              "|:-|:---------------------|:------------------:|:-------:|:-------:|:-------:|:-------:|"]
     number = 1
-    for person in reversed(sorted(all_users.items(), key=lambda item: item[1][0]*100 + item[1][1]*10 + item[1][2] + item[1][3]*0.1)):
-        output.append(f"| {number} | {person[0]} | {all_users[person[0]][0]} | {all_users[person[0]][1]} | {all_users[person[0]][2]} | {all_users[person[0]][3]} | ")
+    for person in reversed(sorted(all_users.items(), key=lambda item: item[1][0]*5000 + item[1][1]*3000 + item[1][2]*1500 + item[1][3]*500)):
+        all_contrib = all_users[person[0]][0]*5000 + all_users[person[0]][1]*3000 + all_users[person[0]][2]*1500 + all_users[person[0]][3]*500
+        output.append(f"| {number} | {person[0]} |  {all_contrib} | {all_users[person[0]][0]} | {all_users[person[0]][1]} | {all_users[person[0]][2]} | {all_users[person[0]][3]} |")
         number += 1
     print(len(weeks))
     local_path = "D:\\Scripts\\RedditAPIScript\\donut-distribution\\"

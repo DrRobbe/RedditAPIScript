4c8e090fd4d5779c0d611c822ccb4849204b047f
DrRobbe <marcelwallraff@gmail.com>
2024-12-23 13:45:20 +0100

    New distribution

--- donut-distribution/lessweight.py
+++ donut-distribution/lessweight.py
@@ -2,8 +2,18 @@ from typing import Dict, List
 import json
 
 
-def create_user(file_name: str) -> Dict[str, List[int]]:
+def create_user(file_name: str, contrib_name: str) -> Dict[str, List[int]]:
     user: Dict[str, List[int]] = {}
+    user_contrib: Dict[str, List[int]] = {}
+    twentyk_contirb_user = []
+    with open(contrib_name) as f:
+        lines = f.readlines()
+        for line in lines:
+            contrib_user = line.split(",")[0]
+            if 'username' != contrib_user:
+                contrib = int(line.split(",")[2])
+                if contrib > 19999:
+                    twentyk_contirb_user.append(contrib_user)
     with open(file_name) as f:
         file_content = json.load(f)
         for values in file_content:
@@ -14,21 +24,28 @@ def create_user(file_name: str) -> Dict[str, List[int]]:
                     if sender not in user:
                         user[sender] = [0, 0]
                     user[sender][0] += 1
+                    if sender not in twentyk_contirb_user:
+                        if sender not in user_contrib:
+                            user_contrib[sender] = [0, 0]
+                        user_contrib[sender][0] += 1
         print(f"{len(user)} user had a tip weight less than 1.")
         for values in file_content:
             if values["to_user_registered"] == 1:
                 receiver = values["to_user"]
                 if receiver in user:
                     user[receiver][1] += 1
-    return user
+                if receiver in user_contrib:
+                    user_contrib[receiver][1] += 1
+    return user, user_contrib
 
 
 if __name__ == "__main__":
     local_path = 'D:\\Scripts\\RedditAPIScript\\donut-distribution\\'
-    distribution = 142
+    distribution = 144
     print(f"For round {distribution}:")
     file_name = f'{local_path}input\\tips_round_{distribution}.json'
-    users = create_user(file_name)
+    contrib_name = f'{local_path}input\\user\\users.000.task_00{distribution - 1}.txt'
+    users, user_contrib = create_user(file_name, contrib_name)
     send = 0
     received = 0
     for user, tips in users.items():
@@ -36,3 +53,12 @@ if __name__ == "__main__":
         received += tips[1]
     print(f'These users send {send} tips and received {received} tips.')
     print(f'On average they get {round(received / send, 2)} tips back for every tip they send.')
+
+    contrib_send = 0
+    contrib_received = 0
+    for user, tips in user_contrib.items():
+        contrib_send += tips[0]
+        contrib_received += tips[1]
+    print(f"{len(user_contrib)} user had a tip weight less than 1. and less than 20k contrib.")
+    print(f'Users with less than 20k contrib send {contrib_send} tips and received {contrib_received} tips.')
+    print(f'On average they get {round(contrib_received / contrib_send, 2)} tips back for every tip they send.')

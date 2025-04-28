60506189f98b6de4b82fa33fa1305e3ecd17ea23
DrRobbe <marcelwallraff@gmail.com>
2025-04-28 06:37:26 +0200

    Week 17

--- donut-distribution/tip_leaderboard.py
+++ donut-distribution/tip_leaderboard.py
@@ -85,9 +85,9 @@ def create_table(users: Dict[str, Dict[str, List[float]]], send_table: bool, dat
 
 
 if __name__ == "__main__":
-    date = datetime.strptime("2025-03-17 00:00:00", '%Y-%m-%d %H:%M:%S')
+    date = datetime.strptime("2025-04-21 00:00:00", '%Y-%m-%d %H:%M:%S')
     print("Check all tips since :" + str(date))
-    distribution = 148
+    distribution = 149
     file_name = local_path + f'input\\tips_round_{distribution}.json'
     user_send, user_receive = create_user(file_name, date)
     # global data
